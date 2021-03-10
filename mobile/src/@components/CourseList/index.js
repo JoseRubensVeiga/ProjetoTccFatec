8aeeb918fbdd1eff2c27eb0ed6ec6dfbab27e384
José <jose.rubens.veiga@gmail.com>
2021-03-10 19:57:26 -0300

    finalizando criação da home

--- mobile/src/@components/CourseList/index.js
+++ mobile/src/@components/CourseList/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
-import image from '../../assets/images/videos/video_01.jpg';
+import image1 from '../../assets/images/videos/video_01.jpg';
+import image2 from '../../assets/images/videos/video_02.png';
+import image3 from '../../assets/images/videos/video_03.jpg';
 
 import {Container, VideoImage, VideoContainer, VideoTitle} from './styles';
 
@@ -8,28 +10,28 @@ const CourseList = () => {
   return (
     <Container horizontal={true} showsHorizontalScrollIndicator={false}>
       <VideoContainer>
-        <VideoImage source={image} />
+        <VideoImage source={image1} />
         <VideoTitle>Iniciando JavaScript</VideoTitle>
       </VideoContainer>
       <VideoContainer>
-        <VideoImage source={image} />
+        <VideoImage source={image2} />
+        <VideoTitle>Matemática na programação</VideoTitle>
+      </VideoContainer>
+      <VideoContainer>
+        <VideoImage source={image3} />
+        <VideoTitle>Dicas de PHP</VideoTitle>
+      </VideoContainer>
+      <VideoContainer>
+        <VideoImage source={image1} />
         <VideoTitle>Iniciando JavaScript</VideoTitle>
       </VideoContainer>
       <VideoContainer>
-        <VideoImage source={image} />
-        <VideoTitle>Iniciando JavaScript</VideoTitle>
+        <VideoImage source={image2} />
+        <VideoTitle>Matemática na programação</VideoTitle>
       </VideoContainer>
       <VideoContainer>
-        <VideoImage source={image} />
-        <VideoTitle>Iniciando JavaScript</VideoTitle>
-      </VideoContainer>
-      <VideoContainer>
-        <VideoImage source={image} />
-        <VideoTitle>Iniciando JavaScript</VideoTitle>
-      </VideoContainer>
-      <VideoContainer>
-        <VideoImage source={image} />
-        <VideoTitle>Iniciando JavaScript</VideoTitle>
+        <VideoImage source={image3} />
+        <VideoTitle>Dicas de PHP</VideoTitle>
       </VideoContainer>
     </Container>
   );
