1f7a9622a14fa93dcfe0fad9666a5051ec25ea45
José <jose.rubens.veiga@gmail.com>
2021-03-19 21:05:13 -0300

    criando o serviço de envio de e-mail de recuperação de senha

--- backend/src/routes/sessions.routes.ts
+++ backend/src/routes/sessions.routes.ts
@@ -1,6 +1,11 @@
 import { Router } from 'express';
 
+import nodemailer from 'nodemailer';
+import { MailOptions } from 'nodemailer/lib/json-transport';
+import emailConfig from '../config/email';
+
 import CreateSessionService from '../services/CreateSessionService';
+import SendRecoveryPasswordEmailService from '../services/SendRecoveryPasswordEmail';
 
 const sessionsRouter = Router();
 
@@ -15,6 +20,24 @@ sessionsRouter.post('/', async (request, response) => {
   });
 
   return response.json({ user, token });
+});
+
+sessionsRouter.post('/recoveryPassword', async (request, response) => {
+  const { email } = request.body;
+
+  if (!email) {
+    return response.status(400).json({
+      message: 'Please, provide a email address',
+    });
+  }
+
+  const sendRecoveryPasswordEmail = new SendRecoveryPasswordEmailService();
+
+  await sendRecoveryPasswordEmail.execute(email);
+
+  return response.json({
+    message: 'email successfully sent',
+  });
 });
 
 export default sessionsRouter;
