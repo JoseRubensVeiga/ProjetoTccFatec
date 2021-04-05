f2182fdb5ddf8c8ca25e1a80143c39baa8a2e081
José <jose.rubens.veiga@gmail.com>
2021-04-05 20:01:28 -0300

    teste deploy

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -8,7 +8,9 @@ const server = express();
 
 server.use(express.json());
 server.use(cors());
-server.use(authRoutes);
+// server.use(authRoutes);
+
+server.get('/', (_, res) => res.json({ ok: true }));
 
 server.listen(3333, () => {
   console.log('Server listening on port 3333! =D');
