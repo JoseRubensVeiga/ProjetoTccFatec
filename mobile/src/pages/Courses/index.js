356d78e192eea35780888241b824ee959e551d9b
José <jose.rubens.veiga@gmail.com>
2021-04-21 14:34:13 -0300

    finalizando a criação da pagina de cursos iniciados

--- mobile/src/pages/Courses/index.js
+++ mobile/src/pages/Courses/index.js
@@ -1,18 +1,34 @@
 import React from 'react';
-import {Text} from 'react-native';
-
-import {Container} from './styles';
+import {ScrollView} from 'react-native';
+import {VideoImage, VideoContainer, VideoTitle} from './styles';
 import Header from '../../@components/Header';
+import PercentLine from '../../@components/PercentLine';
+
+import image1 from '../../assets/images/videos/video_01.jpg';
 
 const Courses = () => {
   return (
-    <Container>
+    <ScrollView>
       <Header
         title="Meus Cursos"
         text="Termine seus cursos e obtenha seu diploma."
       />
-      <Text>Courses</Text>
-    </Container>
+      <VideoContainer>
+        <VideoImage source={image1} />
+        <PercentLine percent={70} />
+        <VideoTitle>Iniciando no JavaScript</VideoTitle>
+      </VideoContainer>
+      <VideoContainer>
+        <VideoImage source={image1} />
+        <PercentLine percent={100} />
+        <VideoTitle>Back-end com .NET Core</VideoTitle>
+      </VideoContainer>
+      <VideoContainer>
+        <VideoImage source={image1} />
+        <PercentLine percent={20} />
+        <VideoTitle>Front-end com Angular</VideoTitle>
+      </VideoContainer>
+    </ScrollView>
   );
 };
 
--- mobile/src/pages/Courses/styles.js
+++ mobile/src/pages/Courses/styles.js
@@ -1,3 +1,19 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+export const VideoImage = styled.Image`
+  width: 300px;
+  height: 160px;
+
+  width: 100%;
+`;
+
+export const VideoContainer = styled.View`
+  margin-left: 15px;
+  margin-right: 15px;
+  margin-top: 10px;
+`;
+
+export const VideoTitle = styled.Text`
+  font-size: 24px;
+  font-weight: bold;
+`;
