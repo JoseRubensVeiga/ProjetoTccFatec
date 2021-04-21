67de4a360dc454173ec434957916f7e90abfd6a8
José <jose.rubens.veiga@gmail.com>
2021-04-21 14:47:05 -0300

    iniciando criação da pagina de favoritos

--- mobile/src/pages/Favorites/index.js
+++ mobile/src/pages/Favorites/index.js
@@ -1,14 +1,33 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {ScrollView} from 'react-native';
 
-import {Container} from './styles';
+import {Container, ChipsContainer, Chip} from './styles';
 import Header from '../../@components/Header';
 
 const Favorites = () => {
   return (
     <Container>
       <Header />
-      <Text>Favorites</Text>
+      <ChipsContainer>
+        <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+          <Chip>JavaScript</Chip>
+          <Chip>PHP</Chip>
+          <Chip>ReactJS</Chip>
+          <Chip>JAVA</Chip>
+          <Chip>Web development</Chip>
+          <Chip>.NET</Chip>
+        </ScrollView>
+        <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+          <Chip>Desktop development</Chip>
+          <Chip>Angular</Chip>
+          <Chip>Vue</Chip>
+          <Chip>React Native</Chip>
+          <Chip>ASP</Chip>
+          <Chip>C#</Chip>
+          <Chip>NodeJS</Chip>
+          <Chip>Express</Chip>
+        </ScrollView>
+      </ChipsContainer>
     </Container>
   );
 };
