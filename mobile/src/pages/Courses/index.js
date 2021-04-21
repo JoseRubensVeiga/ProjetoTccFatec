b6f93b128cbea9811d422ba28c31f2b448d8d57e
José <jose.rubens.veiga@gmail.com>
2021-04-21 13:59:45 -0300

    iniciando a criação da página de cursos

--- mobile/src/pages/Courses/index.js
+++ mobile/src/pages/Courses/index.js
@@ -2,10 +2,15 @@ import React from 'react';
 import {Text} from 'react-native';
 
 import {Container} from './styles';
+import Header from '../../@components/Header';
 
 const Courses = () => {
   return (
     <Container>
+      <Header
+        title="Meus Cursos"
+        text="Termine seus cursos e obtenha seu diploma."
+      />
       <Text>Courses</Text>
     </Container>
   );
--- mobile/src/pages/Favorites/index.js
+++ mobile/src/pages/Favorites/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import {Text} from 'react-native';
 
 import {Container} from './styles';
+import Header from '../../@components/Header';
 
 const Favorites = () => {
   return (
     <Container>
+      <Header />
       <Text>Favorites</Text>
     </Container>
   );
